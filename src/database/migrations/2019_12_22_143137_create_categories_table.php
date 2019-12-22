adfa4041156fb8c27830bfab81c4963e3dfea133
yayoiyamato <45757158+yayoiyamato@users.noreply.github.com>
2019-12-22 14:43:55 +0900

    DBマイグレーション作成

---
+++ src/database/migrations/2019_12_22_143137_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 100);	
+            $table->string('slug', 100);	
+            $table->integer('order')->nullable()->unsigned();
+            $table->boolean('is_show')->default(1);
+            $table->timestamps(0);
+            $table->softDeletesTz('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ src/database/migrations/2019_12_22_143138_create_memos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('title', 100);	
+            $table->integer('category_id')->nullable();
+            $table->string('type', 2);	
+            $table->longText('text');
+            $table->boolean('is_show')->default(1);
+            $table->timestamps(0);
+            $table->softDeletesTz('deleted_at');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
