bb25db675d387bce87e43fd13d4643e2da777111
yayoiyamato <45757158+yayoiyamato@users.noreply.github.com>
2019-12-22 17:05:04 +0900

    sidemenu追加

---
+++ src/resources/views/front/globals/sidemenu.blade.php
@@ -0,0 +1,6 @@
+<h2>サイドメニュー</h2>
+<ul>
+    <li><a href="/"><i class="far fa-file-alt"></i> めも</a></li>
+    <li><a href="/add"><i class="far fa-edit"></i> めもを追加</a></li>
+    <li><a href="/categories"><i class="far fa-folder-open"></i> カテゴリ</a></li>
+</ul>
--- src/resources/views/front/layouts/default.blade.php
+++ src/resources/views/front/layouts/default.blade.php
@@ -2,11 +2,13 @@
     <head>
         <title>{{ $title }}</title>
         <meta charset="UTF-8">
+        <link href='//use.fontawesome.com/releases/v5.6.1/css/all.css' rel='stylesheet'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
     </head>
     <body>
         @include('front.globals.header')
+        @include('front.globals.sidemenu')
         <div class="container">
             @yield('content')
         </div>
